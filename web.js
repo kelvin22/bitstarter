85b4ef37985e0cc746828317106065124af4dd8b
Kelvin Lau <kelvin22@gmail.com>
2013-07-09 21:34:39 +1000

    added index.html from class

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  	indextext = fs.readFileSync('index.html');
+	var buf = new Buffer(indextext)
+	response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
